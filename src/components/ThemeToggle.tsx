42b81b3f350048a234785942557347be0022408e
KrazyManJ <KrazyManJ@gmail.com>
2023-12-16 21:29:21 +0100

    Added lucide dep, updated framer-motion and next deps, updated theme toggle button

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -3,6 +3,8 @@
 import React, {useEffect, useState} from 'react';
 import {useTheme} from "next-themes";
 import ContaineredSquircle from "@/components/ContaineredSquircle";
+import {Moon, Sun } from 'lucide-react';
+import { motion } from 'framer-motion';
 
 const ThemeToggle = () => {
     const {systemTheme, theme, setTheme} = useTheme()
@@ -17,16 +19,32 @@ const ThemeToggle = () => {
         return theme == 'dark' ? 'light' : 'dark';
     }
 
+    const motionVariants = {
+        pressed: {scale: 0.7},
+        default: {scale: 1}
+    }
+
     return (
+        <motion.div
+            variants={motionVariants}
+            transition={{type: "spring",bounce: 0.7}}
+            whileHover={{scale: 1.1}}
+            whileTap={{scale: 0.95, transition: {duration: 0.05}}}
+            className={"w-fit"}
+        >
+
         <ContaineredSquircle
             onClick={() => setTheme(toggle())}
-            cornerRadius={10}
+            cornerRadius={12}
             cornerSmoothing={1}
-            containerClassName={"shadow w-fit"}
+            containerClassName={"drop-shadow-lg w-fit"}
             className={"w-fit dark:bg-white bg-black transition-colors text-white dark:text-black"}
         >
-            <button className={"p-2"}>Toggle to {toggle()} theme</button>
+            <button className={"p-2"}>
+                {toggle() == 'light' ? <Sun/> : <Moon/>}
+            </button>
         </ContaineredSquircle>
+        </motion.div>
     );
 };
 
